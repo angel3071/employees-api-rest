065b78960521fffcfe276b5a4926a5c10d283281
Angel <aramirez@voicetechsolutions.com.mx>
2019-05-20 17:36:51 -0500

    intitial version for GetterEmployees, needs refactoring

--- src/main/java/com/angel/employees/GetterEmployees.java
+++ src/main/java/com/angel/employees/GetterEmployees.java
@@ -2,9 +2,7 @@ package com.angel.employees;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -12,11 +10,10 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import org.json.simple.JSONObject;
 
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 
 
-public class Getter implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class GetterEmployees implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
     static DynamoDB dynamoDB = new DynamoDB(client);
@@ -27,32 +24,39 @@ public class Getter implements RequestHandler<APIGatewayProxyRequestEvent, APIGa
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("Loading Java Lambda handler of Employees Getter");
+        logger.log("Loading Java Lambda handler of Employees GetterEmployees");
 
 
 
-        String employeeId = "";
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
         Table table = dynamoDB.getTable(tableName);
         try {
 
-            Map<String, String> qps = event.getPathParameters();
-            if (qps != null) {
-                if (qps.get("id") != null) {
-                    employeeId = qps.get("id");
-                }
+
+            Map<String, Object> expressionAttributeValues = new HashMap<String, Object>();
+            expressionAttributeValues.put(":s", "ACTIVE");
+            Map<String, String> expressionAttributeNames = new HashMap<>();
+            expressionAttributeNames.put("#st", "Status");
+            ItemCollection<ScanOutcome> items = table.scan(
+                    "#st = :s",
+                    "Id, FirstName",
+                    expressionAttributeNames,
+                    expressionAttributeValues);
+            logger.log("Items accumulatedItemCount: " + items.getAccumulatedItemCount());
+            String responseBodyString = "[\n";
+            Iterator<Item> iterator = items.iterator();
+            while(iterator.hasNext()){
+                responseBodyString += iterator.next().toJSONPretty() + ", ";
             }
-            logger.log("EmployeeId: " + employeeId);
-            Item item = table.getItem("Id", employeeId);
-            if(item.get("Status") != null && "ACTIVE".equals(item.get("Status"))){
+            responseBodyString += "\n]";
+            if(responseBodyString.length() > 0 ){
                 response.setStatusCode(200);
-                String responseBodyString = item.toJSONPretty();
                 response.setBody(responseBodyString);
             } else {
                 response.setStatusCode(400);
                 Map<String, String> responseBody = Collections.singletonMap("message", "Employee not found");
-                String responseBodyString = new JSONObject(responseBody).toJSONString();
+                responseBodyString = new JSONObject(responseBody).toJSONString();
                 response.setBody(responseBodyString);
             }
 
